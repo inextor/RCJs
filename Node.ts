bb41c00a8641d9fad2773fe9e0cefb1b79666456
nextor <nextor@gmail.com>
2023-02-17 01:26:19 -0800

    Doesnt work

--- Node.ts
+++ Node.ts
@@ -1,377 +1,422 @@
 
 type half_byte=0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15;
-type fcallback=(node:Node,value:half_byte)=>void;
-interface NodeValue{
-    value:number;
-    node:Node;
+type fcallback=(node:CNode,value:half_byte)=>void;
+interface CNodeValue{
+	value:number;
+	node:CNode;
 };
 
-export class Node
+class CNode
 {
-    value:half_byte= 0;
-    node_dictionary:Node[] = [];
-    counter:number = 0;
-    is_marked:boolean = false;
+	value:number= 0;
+	node_dictionary:CNode[] = [];
+	counter:number = 0;
+	is_marked:boolean = false;
+	name:string;
 
-    constructor(value:half_byte) 
-    {
-        this.node_dictionary = Node.buildDictionary();
-        this.foreach((n,i)=>n.setNodeFor(i,this));
-    }
+	constructor(value:number)
+	{
+		this.value =  value;
+		this.node_dictionary = new Array(16);
+		this.node_dictionary.fill( this );
+	}
 
-    public getNode(value:half_byte):Node
-    {
-        if( this.node_dictionary.length == 0 )
-            throw new Exception('Error dictionary is empty');
+	public getCNode(value:number):CNode
+	{
+		if( this.node_dictionary.length == 0 )
+			throw 'Error dictionary is empty';
 
-        return this.node_dictionary.find(n=>n.value == value) as Node;
-    }
+		return this.node_dictionary.find(n=>n.value == value) as CNode;
+	}
 
-    public unmark()
-    {
-        if( !this.is_marked)
-            return;
+	public unmark()
+	{
+		if( !this.is_marked)
+			return;
 
-        this.is_marked = false;
+		this.is_marked = false;
 
-        for(let z in this.node_dictionary)
-        {
-            z.unMark();
-        }
-    }
-    public resetCounters()
-    {
-        if( this.is_marked )
-            return;
+		for(let z of this.node_dictionary)
+		{
+			z.unmark();
+		}
+	}
+	public resetCounters()
+	{
+		if( this.is_marked )
+			return;
 
-        this.is_marked = true;
-        
-        this.counter = 0;
+		this.is_marked = true;
 
-        for(let z in this.node_dictionary)
-            z.resetCounters(data);
-    }
+		this.counter = 0;
 
-    public sumCounter(data:number[]):void
-    {
-        if( this.is_marked )
-            return;
+		for(let z of this.node_dictionary)
+			z.resetCounters();
+	}
 
-        this.is_marked = true;
-        
-        data[this.value]+= this.counter; 
-        for(let z in this.node_dictionary)
-            z.sumCounter(data);
-    }
+	public sumCounter(data:number[]):void
+	{
+		if( this.is_marked )
+			return;
 
-    public addCounter(data:number[])
-    {
-        if( this.is_marked )
-            return;
+		this.is_marked = true;
 
-        this.is_marked = true;
-        
-        data.push( this.counter );
+		data[this.value]+= this.counter;
+		for(let z of this.node_dictionary)
+			z.sumCounter(data);
+	}
 
-        for(let z in this.node_dictionary)
-            z.addCounter(data);
-    }
+	public addCounter(data:number[])
+	{
+		if( this.is_marked )
+			return;
 
-    compare(node:Node)
-    {
-        return node.counter > this.counter ? 1 : -1;
-    }
+		this.is_marked = true;
 
-    public sortPositions()
-    {
-        if( this.is_marked )
-            return;
+		data.push( this.counter );
 
-        this.node_dictionary.sort((n1,n2)=>n1.compare(n2));
-        
-        for(let z in this.node_dictionary)
-        {
-            z.sortPositions();
-        }
-    }
+		for(let z of this.node_dictionary)
+			z.addCounter(data);
+	}
+
+	compare(node:CNode)
+	{
+		return node.counter > this.counter ? 1 : -1;
+	}
+
+	public sortPositions()
+	{
+		this.node_dictionary.sort((n1,n2)=>n1.compare(n2));
+
+		if( this.is_marked )
+			return;
+
+		this.is_marked = true;
 
 
-    public analyze(value:half_byte){
-        this.counter++;
-        return {
-            value: this.value,
-            node: this.node_dictionary[value]
-        }
-    }
+		for(let z of this.node_dictionary)
+		{
+			z.sortPositions();
+		}
+	}
 
-    public process():half_byte
-    {
-        return this.value;
-    }
+	public analyze(value:number){
+		this.counter++;
+		return {
+			value: this.value,
+			node: this.node_dictionary[value]
+		}
+	}
 
-    public getNextNodeAnValue(value:half_byte):NodeValue
-    {
-        let node = this.node_dictionary[value];
-        return { node: node, value: this.value }
-    }
+	public process():number
+	{
+		return this.value;
+	}
 
-    setNodeFor(value:half_byte,node:Node):void
-    {
-        this.node_dictionary[value] = node;   
-    }
+	public compress(value:number)
+	{
+		let index = this.node_dictionary.findIndex(n=>n.value==value) as number;
+		let node = this.node_dictionary[index];
+		return { node: node, value: index }
+	}
 
-    foreach(f:fcallback):void
-    {
-        this.node_dictionary.forEach(f);
-    }
+	public getNextCNodeAnValue(value:number):CNodeValue
+	{
+		let index = this.node_dictionary.findIndex(n=>n.value==value) as number;
+		let node = this.node_dictionary[value];
+		return { node: node, value: index }
+	}
 
-    static buildDictionary():Node[]
-    {
-        let n:Node[] = new Array(16);
-        for(let i=0;i<16;i++)
-        {
-            n[i] = new Node(i);
-        }
-        return n;
-    }
-    //1+16 =  17*49 = 833 bytes dictionary
-    static build16Plus256():Node[]
-    {
-        let n:Node[] = Node.buildDictionary();
-        n.forEach((node,j)=>
-        {
-            let d = Node.buildDictionary();
-            node.foreach((n2,j)=>{
-                n2.setNodeFor(j,d[j]);
-                d[j].foreach((n3,k)=>{
-                    n3.setNodeFor(k,n[k]);
-                })
-            })
-        })
+	setCNodeFor(value:number,node:CNode):void
+	{
+		this.node_dictionary[value] = node;
+	}
 
-        for(let i=0;i<16;i++)
-        {
-            n[i] = new Node(i);
-            n[i].foreach((node,j)=>{
-                let d = Node.buildDictionary();
-                node.foreach((n2,j)=>{
-                    n2.setNodeFor(j,d[j]);
-                    d[j].foreach((n3,k)=>{
-                        n3.setNodeFor(k,n[k]);
-                    })
-                })
-            })
-        }
-       return n;
-    }
-    // 16+16) * 49 = 1568 bytes dictionary
-    // 16+16*16
-    static build16Plus16():Node[]
-    {
-        let d1 = Node.buildDictionary();
-        let d2 = Node.buildDictionary();
+	foreach(f:fcallback):void
+	{
+		this.node_dictionary.forEach(f);
+	}
 
-        d1.forEach((n,i)=>n.setNodeFor(i,d2[i]));
-        d2.forEach((n,i)=>n.setNodeFor(i,d1[i]));
-        return d1;
-    }
+	static buildDictionary():CNode[]
+	{
+		let n:CNode[] = new Array(16);
+		for(let i=0;i<16;i++)
+		{
+			n[i] = new CNode(i);
+		}
+		return n;
+	}
+	////1+16 = 17*49 = 833 bytes dictionary
+	//static build16Plus256():CNode[]
+	//{
+	//	let n:CNode[] = CNode.buildDictionary();
+	//	n.forEach((node,j)=>
+	//	{
+	//		let d = CNode.buildDictionary();
+	//		node.foreach((n2,j)=>{
+	//			n2.setCNodeFor(j,d[j]);
+	//			d[j].foreach((n3,k)=>{
+	//				n3.setCNodeFor(k,n[k]);
+	//			})
+	//		})
+	//	})
 
-    static build16P16X4():Node[][]
-    {
-        let d1 = Node.buildDictionary();
-        let d2 = Node.buildDictionary();
-        let d3 = Node.buildDictionary();
-        let d4 = Node.buildDictionary();
+	//	for(let i=0;i<16;i++)
+	//	{
+	//		n[i] = new CNode(i);
+	//		n[i].foreach((node,j)=>{
+	//			let d = CNode.buildDictionary();
+	//			node.foreach((n2,j)=>{
+	//				n2.setCNodeFor(j,d[j]);
+	//				d[j].foreach((n3,k)=>{
+	//					n3.setCNodeFor(k,n[k]);
+	//				})
+	//			})
+	//		})
+	//	}
+	//	return n;
+	//}
+	// 16+16) * 49 = 1568 bytes dictionary
+	// 16+16*16
+	static build16Plus16():CNode[]
+	{
+		let d1 = CNode.buildDictionary();
+		let d2 = CNode.buildDictionary();
 
-        d1.forEach((n,i)=>n.setNodeFor(i,d2[i]));
-        d2.forEach((n,i)=>n.setNodeFor(i,d3[i]));
-        d3.forEach((n,i)=>n.setNodeFor(i,d4[i]));
-        d4.forEach((n,i)=>n.setNodeFor(i,d1[i]));
-        return [d1,d2,d3,d4];
-    }
+		d1.forEach((n,i)=>n.setCNodeFor(i,d2[i]));
+		d2.forEach((n,i)=>n.setCNodeFor(i,d1[i]));
+		return d1;
+	}
 
-    static build16PxN(l):Node[][]
-    {
-        let levels:Node[][] = [];
-        let d1 = Node.buildDictionary();
+	static build16P16X4():CNode[][]
+	{
+		let d1 = CNode.buildDictionary();
+		let d2 = CNode.buildDictionary();
+		let d3 = CNode.buildDictionary();
+		let d4 = CNode.buildDictionary();
 
-        for(let i=0;i<levels;i++)
-        {
-            levels[i] = Node.buildDictionary();
-        }
+		d1.forEach((n,i)=>n.setCNodeFor(i,d2[i]));
+		d2.forEach((n,i)=>n.setCNodeFor(i,d3[i]));
+		d3.forEach((n,i)=>n.setCNodeFor(i,d4[i]));
+		d4.forEach((n,i)=>n.setCNodeFor(i,d1[i]));
+		return [d1,d2,d3,d4];
+	}
 
-        for(let i=0;i<levels;i++)
-        {
-            levels[i].forEach((n,index)=>{
-                if( i<l-1 )
-                {
-                    n.setNodeFor(index,levels[i+1])
-                }
-                else
-                {
-                    n.setNodeFor(index,levels[0]);
-                }
-            })
-        }
+	static build16PxN(l:number):CNode[][]
+	{
+		let levels:CNode[][] = new Array(l);
 
-        return levels;
-    }
+		for(let i=0;i<levels.length;i++)
+		{
+			levels[i] = CNode.buildDictionary();
+		}
 
-    static build16Plus256():Node[]
-    {
-        let d1 = Node.buildDictionary();
-        for(let n of d1)
-        {
-            let d2 = Node.buildDictionary();
-            let hb = 0;
-            for(let nn of d2)
-            {
-                n.setNodeFor(nn.value,nn);
-                let d3 = Node.buildDictionary();
-                for(let nnn of d3)
-                {
-                    nn.setNodeFor(nnn.value,nnn);
-                    for(let fn of d1)
-                    {
-                        nnn.setNodeFor(fn.value,d1[fn.value]);
-                    }
-               }
-            }
-        }
-        return d1;
-    }
+		for(let i=0;i<levels.length;i++)
+		{
+			levels[i].forEach((n,index)=>{
+				for(let kk=0;kk<16;kk++)
+				{
+					if( i<l-1 )
+					{
+						n.setCNodeFor(kk,levels[i+1][kk]);
+					}
+					else
+					{
+						n.setCNodeFor(kk,levels[0][kk]);
+					}
+				}
+			})
+		}
 
-    static build16Plus16():Node[]
-    {
-        let d1 = Node.buildDictionary();
-        let d2 = Node.buildDictionary();
-        let d3 = Node.buildDictionary();
-        let d4 = Node.buildDictionary();
+		return levels;
+	}
 
-        d1.forEach((n,i)=>n.setNodeFor(i,d2[i]));
-        d2.forEach((n,i)=>n.setNodeFor(i,d1[i]));
+	static build16Plus256():CNode[]
+	{
+		let d1 = CNode.buildDictionary();
+		console.log('BUilding d1');
 
-    }
-    static build16Plus32()
-    {
-        let d1 = Node.buildDictionary();
-        let d2 = Node.buildDictionary();
-        let d3 = Node.buildDictionary();
-        
-        d1.forEach((node,half_byte)=>{            
-            node.setNodeFor(half_byte,half_byte<8?d2[half_byte]:d3[half_byte]);
-        })
-    }
+		for(let n of d1)
+		{
+			n.name = 'd1';
+			let d2 = CNode.buildDictionary();
+			console.log('BUilding d2');
+			for(let nn of d2)
+			{
+				console.log('Setting for d1', nn.value, nn);
+				n.setCNodeFor(nn.value,nn);
+				nn.name = 'd2';
+				let d3 = CNode.buildDictionary();
+				//console.log('BUilding d3');
+				for(let nnn of d3)
+				{
+					nnn.name = 'd3';
+					nn.setCNodeFor(nnn.value,nnn);
+					for(let fn of d1)
+					{
+						nnn.setCNodeFor(fn.value,fn);
+					}
+				}
+			}
+		}
+		return d1;
+	}
+
+	//static build16Plus16():CNode[]
+	//{
+	//	let d1 = CNode.buildDictionary();
+	//	let d2 = CNode.buildDictionary();
+	//	let d3 = CNode.buildDictionary();
+	//	let d4 = CNode.buildDictionary();
+
+	//	d1.forEach((n,i)=>n.setCNodeFor(i,d2[i]));
+	//	d2.forEach((n,i)=>n.setCNodeFor(i,d1[i]));
+
+	//}
+	//static build16Plus32()
+	//{
+	//	let d1 = CNode.buildDictionary();
+	//	let d2 = CNode.buildDictionary();
+	//	let d3 = CNode.buildDictionary();
+	//
+	//	d1.forEach((node,half_byte)=>{
+	//		node.setCNodeFor(half_byte,half_byte<8?d2[half_byte]:d3[half_byte]);
+	//	})
+	//}
 
 
-    //Interesting next schema
-    //its a triangle
-    /*
-        d1->d2
-        d2->d3
-        d3->d1
-        d1->dd1
-        d2->dd2
-        d3->dd3
-        dd1->dd2
-        dd2->dd3
-        dd3->dd1
-        dd1->d3
-        dd2->d1
-        dd3->d2
-    */
+	//Interesting next schema
+	//its a triangle
+	/*
+		d1->d2
+		d2->d3
+		d3->d1
+		d1->dd1
+		d2->dd2
+		d3->dd3
+		dd1->dd2
+		dd2->dd3
+		dd3->dd1
+		dd1->d3
+		dd2->d1
+		dd3->d2
+	*/
 }
 
-function compress(node, data:Uint8Array[])
+function compress(node:CNode, data:number[]):number[]
 {
-    let current = node;
-    let u_data = new Uint8Array(data.length);
-    let fh:number = 0;
-    let sh:number = 0;
+	let current = node;
+	let u_data:number[] = new Array(data.length);
+	let counter = new Array(16);
+	counter.fill(0);
 
-    let index = 0;
-    for(let d in data)
-    {
-        fh = d >> 4;
-        sh = d & 0x0f;
+	let index = 0;
 
-        let x = current.getNextNodeAnValue(fh)
-        current = x.node;
-        let y  = current.getNextNodeAnValue(sh);
-        current = y.node;
-        fh = x.value << 4;
-        nData[index++] = fh | y.value;
-    }
-    return u_data;
+	let prev = 0;
+
+	for(let d of data)
+	{
+		let diff = prev - d;
+		if( diff < 0 )
+			diff+=16;
+
+		let x = current.compress(diff)
+		counter[ x.value ]++;
+		u_data[index++] = x.value;
+		prev = d;
+	}
+	return counter;
  }
 
-function uncompress(node, data:Uint8Array[])
-{
-    let current = node;
-    let u_data = new Uint8Array(data.length);
-    let fh:number = 0;
-    let sh:number = 0;
+//function uncompress(node:CNode, data:number[])
+//{
+//	let current = node;
+//	let u_data = new Uint8Array(data.length);
+//	let fh:number = 0;
+//	let sh:number = 0;
+//
+//	let index = 0;
+//	for(let d of data)
+//	{
+//		fh = d >> 4;
+//		sh = d & 0x0f;
+//
+//		let x = current.getNextCNodeAnValue(fh )
+//		current = x.node;
+//		u_data[index++] =
+//	}
+//	return u_data;
+// }
 
-    let index = 0;
-    for(let d in data)
-    {
-        fh = d >> 4;
-        sh = d & 0x0f;
-
-        let x = current.getNextNodeAnValue(fh)
-        current = x.node;
-        let y  = current.getNextNodeAnValue(sh);
-        current = y.node;
-        fh = x.value << 4;
-        nData[index++] = fh | y.value;
-    }
-    return u_data;
- }
-
- function analyze(node:Node, data:Uint8Array)
+ function analyze(node:CNode, data:number[])
  {
-    let current = node;
-    let fh:number = 0;
-    let sh:number = 0;
+	let current = node;
+	let fh:number = 0;
+	let sh:number = 0;
+	let prev = 0;
 
-    let index = 0;
-    for(let d in data)
-    {
-        fh = d >> 4;
-        sh = d & 0x0f;
+	for(let d of data)
+	{
+		let diff = prev - d;
+		if( diff < 0 )
+			diff += 16;
 
-        let x = current.analyze(fh)
-        current = x.node;
-        let y  = current.analyze(sh);
-        current = y.node;
-    }
-    return u_data;
+		let x = current.analyze( diff )
+		current = x.node;
+		prev = d;
+	}
  }
 
 
  function main_alg(data:Uint8Array | number[])
  {
-    let d = Node.build16PxN(17)
-    let f_node =d[0][0];
+	let d = CNode.build16Plus256();
+	//let zz = CNode.build16PxN(2);
 
-    analyze(f_node,data);
-    let counters = new Array(16);
-    counters.fill(0);
-    f_node.sumCounter(counters);
-    f_node.unmark();
-    let d:number[] = [];
-    f_node.addCounter(d);
-    console.log( d );
-    f_node.unmark();
-    f_node.sortPositions();
-    let d = compress(f_node,data);
+	//let f_node =zz[0][0];
+	let f_node =d[0];
 
-    let dd = new Array(256);
-    dd.fill(0);
-    for(let x of d)
-    {
-        dd[x]++;
-    }
-    dd.sort();
-    console.log('Byte counter',dd); 
- }
+	console.log('First', f_node );
+
+
+	let halfs:number[] = [];
+	data.forEach((i:number)=>{
+		halfs.push( i>>4, i&15);
+	});
+
+	analyze(f_node,halfs);
+	f_node.sortPositions();
+	//d.sort((a,b)=>{
+	//	return a.compare(b);
+	//});
+
+
+
+
+	let compressed = compress(f_node,halfs);
+
+	let counters = new Array(16);
+	counters.fill(0);
+	f_node.unmark();
+	f_node.sumCounter(counters);
+	f_node.unmark();
+	console.log('Sum counters', counters );
+
+	f_node.unmark();
+	let add_counter:number[] = [];
+	f_node.addCounter(add_counter);
+	console.log('Add counters', add_counter );
+	f_node.unmark();
+
+	console.log('Counters compressed', compressed );
+
+	//let dd = new Array(256);
+	//dd.fill(0);
+	//for(let x of compressed)
+	//{
+	//	dd[x]++;
+	//}
+	//dd.sort();
+	//console.log('Byte counter',dd);
+	console.log('AN 256');
+}
